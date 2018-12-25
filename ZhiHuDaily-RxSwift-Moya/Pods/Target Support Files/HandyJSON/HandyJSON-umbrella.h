43ab037ae0bd5fcd52dd445dbe6d682ff15f7f14
Xiaoye__220 <576934532@qq.com>
2018-12-25 13:02:09 +0800

    update .gitignore

--- ZhiHuDaily-RxSwift-Moya/Pods/Target Support Files/HandyJSON/HandyJSON-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "HandyJSON.h"
-
-FOUNDATION_EXPORT double HandyJSONVersionNumber;
-FOUNDATION_EXPORT const unsigned char HandyJSONVersionString[];
-
--- ZhiHuDaily-RxSwift-Moya/Pods/Target Support Files/Pods-ZhiHuDaily_Swift/Pods-ZhiHuDaily_Swift-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_ZhiHuDaily_SwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ZhiHuDaily_SwiftVersionString[];
-
